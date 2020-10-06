55cb515d27f8d377d8432519fff8ed139376d84c
Darren Ldl <darrenldldev@gmail.com>
2020-10-07 02:56:55 +1100

    Adjusting config

--- bw-script-gen/src/config.ml
+++ bw-script-gen/src/config.ml
@@ -1,6 +1,6 @@
-let jails_dir = "$HOME/jails"
+let jails_dir = "$HOME/sandboxes"
 
-let jail_logs_dir = "$HOME/jail-logs"
+let jail_logs_dir = "$HOME/sandbox-logs"
 
 let stdout_log_suffix = "stdout"
 
@@ -8,7 +8,7 @@ let stderr_log_suffix = "stderr"
 
 let log_date_format_str = "+%Y-%m-%d_%H%M%S"
 
-let home_inside_jail = "/home/jail"
+let home_inside_jail = "/home/sandbox"
 
 let script_output_dir = "../bw-scripts"
 
