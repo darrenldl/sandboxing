befd5ef6b160c28a800c44caaa5debadc7d34ab8
Darren Ldl <darrenldldev@gmail.com>
2020-10-07 02:46:24 +1100

    Refactoring add_links.sh

--- add_links.sh
+++ add_links.sh
@@ -4,15 +4,20 @@ prefix="sandbox"
 
 dst=$1
 
+cur_path=$(dirname $(readlink -f "$0"))
+
 if [[ "$dst" == "" ]]; then
   echo "Please specify destination"
   exit 1
 fi
 
-for file in bw-scripts/*.sh; do
+for file in "$cur_path"/bw-scripts/*.sh; do
   name=$(basename $file | sed 's/\.sh//g')
 
   echo "Creating symbolic link for" $name
 
-  ln -s $(realpath "$file") "$dst"/"$prefix"-"$name"
+  dst_file="$dst"/"$prefix"-"$name"
+
+  rm -f "$dst_file"
+  ln -s $(realpath "$file") "$dst_file"
 done
