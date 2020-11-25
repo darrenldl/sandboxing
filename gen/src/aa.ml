ee8f8ae3311d695437a8ac116934218dcc983da0
Darren Ldl <darrenldldev@gmail.com>
2020-11-25 20:00:53 +1100

    Progress on aa profile

--- gen/src/aa.ml
+++ gen/src/aa.ml
@@ -2,3 +2,9 @@ type capability =
   | Sys_admin
   | Sys_chroot
   | Sys_ptrace
+
+let string_of_capability c =
+  match c with
+  | Sys_admin -> "sys_admin"
+  | Sys_chroot -> "sys_chroot"
+  | Sys_ptrace -> "sys_ptrace"
