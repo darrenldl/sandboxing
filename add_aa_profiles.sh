5e746e3b165073e921043e04fafa50c842d2abdc
Darren Ldl <darrenldldev@gmail.com>
2021-05-23 19:37:20 +1000

    Fixed add_aa_profiles.sh

--- add_aa_profiles.sh
+++ add_aa_profiles.sh
@@ -2,11 +2,14 @@
 
 script_dir=$(dirname $(readlink -f "$0"))
 
+dst=$1
+
 if [[ "$dst" == "" ]]; then
   echo "Please specify destination"
   exit 1
 fi
 
 for file in "$script_dir"/aa-profiles/*; do
+  dst_file="$dst"/$(basename "$file")
   cp $(realpath "$file") "$dst_file"
 done
